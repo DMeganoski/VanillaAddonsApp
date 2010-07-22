8b77e5e4db824df4236893ba5a99e4c6b5450c37
Mark O'Sullivan <mark@vanillaforums.com>
2010-07-22 12:28:43 -0400

    Added comments.

--- js/get.js
+++ js/get.js
@@ -1,4 +1,8 @@
 jQuery(document).ready(function($) {
+   /*
+     Used for prompting download of addons & other files.
+     Redirects to the current url with an additional /1/ parameter
+   */
    var loc = location.href;
    if (loc.substring(0,-1) != '/')
       loc += '/';
