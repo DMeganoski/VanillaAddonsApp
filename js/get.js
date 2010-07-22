bbea3a3e8c83161cbeeb0f3e3cf7cf23af3c3c8e
Todd Burry <todd@vanillaforums.com>
2010-07-22 16:11:45 -0400

    Added a 3 second timeout to downloads so that images and css have time to load first.

--- js/get.js
+++ js/get.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($) {
-   var loc = location.href;
-   if (loc.substring(0,-1) != '/')
-      loc += '/';
-   
-   loc += '1';
-   document.location = loc;
+   setTimeout(function() {
+      var loc = location.href;
+      if (loc.substring(0,-1) != '/')
+         loc += '/';
+
+      loc += '1';
+      document.location = loc;
+   }, 3000);
 });
